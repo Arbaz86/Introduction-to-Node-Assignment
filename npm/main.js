d597fc63d5b271c6f50ee2eff2c85c68149b05a0
Arbaz86 <arbaz10042002@gmail.com>
2022-03-07 01:25:50 +0530

    added Assignments

---
+++ npm/main.js
@@ -0,0 +1,11 @@
+const { add } = require("./add.js");
+const { subtract } = require("./subtract.js");
+const { multiply } = require("./multiply.js");
+const { divide } = require("./divide.js");
+
+console.log("after add 10,5, the Answer is ", add(10, 5));
+console.log("afetr subtract 10,5, the Answer is ", subtract(10, 5));
+console.log("after multiply 10,5, the Answer is ", multiply(10, 5));
+console.log("after divide 10,5, the Answer is ", divide(10, 5));
+
+
